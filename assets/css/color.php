e2015c0c200ce8685f55047215e4f7bf23f6335a
minion <minionhub.dev@gmail.com>
2023-10-11 18:22:15 -0700

    updated

--- assets/css/color.php
+++ assets/css/color.php
@@ -1,30 +1,272 @@
-<?php
-header("Content-Type:text/css");
-$color = "#f0f"; // Change your Color Here
-$secondColor = "#ff8"; // Change your Color Here
 
-function checkhexcolor($color){
-    return preg_match('/^#[a-f0-9]{6}$/i', $color);
+<style>
+
+#phone:focus {
+border-color: #f74a05 !important;
 }
 
-if (isset($_GET['color']) AND $_GET['color'] != '') {
-    $color = "#" . $_GET['color'];
+.custom_border {
+    border-bottom: 1px solid #f74a054f;
 }
 
-if (!$color OR !checkhexcolor($color)) {
-    $color = "#336699";
+.custom_border_span {
+    border: 1px solid #f74a05;
 }
 
-
-function checkhexcolor2($secondColor){
-    return preg_match('/^#[a-f0-9]{6}$/i', $secondColor);
+.bg-dashborad-card {
+background-color: #f74a05;
 }
 
-if (isset($_GET['secondColor']) AND $_GET['secondColor'] != '') {
-    $secondColor = "#" . $_GET['secondColor'];
+.custom--table .thead-dark th {
+background: #f74a05;
+}
+.custom--table .thead-dark th:last-child {
+border-right: 1px solid #f74a05;
+}
+.table td, .table th {
+border-top: 1px solid #f74a0530;
 }
 
-if (!$secondColor OR !checkhexcolor2($secondColor)) {
-    $secondColor = "#336699";
+.site-header .header-top-navbar .info > li > span {
+color: #f74a05;
 }
-?>
+.site-header .header-top-navbar .info > li {
+border-right: 2px dotted #f74a05;
+}
+
+.site-header .header-top-navbar .header-social-share > li > a:focus, .site-header .header-top-navbar .header-social-share > li > a:hover {
+color: #f74a05;
+}
+.site-header .header-top-navbar .user-registration-area .user-reg-btn {
+background-color: #f74a05;
+}
+
+.menu > ul > li > a:before {
+background-color: #f74a054d;
+}
+
+.menu > ul > li > ul.dropdown-content > li:hover > a {
+color: #f74a05 !important;
+}
+.search-wrap .search-btn {
+border: 1px solid #f74a05;
+}
+.search-wrap .search-btn:hover {
+background-color: #f74a05;
+}
+
+.btn-default {
+border: 1px solid #f74a05;
+background-color: #f74a05;
+}
+.btn-default:hover, .btn-default:focus {
+background-color: #f74a05;
+color: white !important;
+}
+.btn-white {
+color: white;
+}
+
+a:hover, a:focus, a:active {
+color: #f74a05;
+}
+
+.search-wrap .search-form button, .search-wrap .search-form input[type="button"], .search-wrap .search-form input[type="reset"], .search-wrap .search-form input[type="submit"] {
+background-color: #f74a05;
+}
+
+.loader {
+border: 7px double #f74a05;
+}
+.loader-4:before, .loader-4:after {
+background: #f74a05;
+}
+
+.section-title .title-main:before {
+background-color: #f74a05;
+}
+
+.single-auction-cat .cat-icon {
+color: #f74a05;
+}
+.single-auction-cat:hover {
+border-color: #f74a054d;
+}
+.auction-item:hover {
+box-shadow: 0px 0px 10px #f74a0540;
+}
+.auction-item {
+box-shadow: 0px 0px 5px #f74a051a;
+border: 1px solid #f74a0524;
+}
+
+.auction-item .bid-max {
+color: #f74a05;
+}
+.auction-item .item-footer .item-footer-top .bid-timer {
+color: #f74a059e;
+}
+
+.topbutton {
+background-color: #f74a05;
+}
+
+.winner-item .winner-thumb > img {
+border-color: #f74a05;
+}
+.winner-item .winner-thumb .product-icon {
+background-color: #f74a05;
+}
+
+.win-steps-block .card-center .card-icon:before {
+border: 1px dashed #f74a05;
+}
+.win-steps-block .card-center .card-icon .step {
+background-color: #f74a05;
+}
+
+.testimonial-item .client-info:before {
+background-color: #f74a05;
+}
+.testimonial-item .icon {
+color: #f74a054d;
+}
+
+.work-brand-block .brands-link {
+border: 1px solid #f74a054d;
+}
+
+.site-footer {
+border-top: 3px solid #f74a05;
+}
+.widget .widget-title:before {
+background-color: #f74a05;
+}
+.widget .contact-info .icon, .menu > ul > li > a:hover {
+color: #f74a05;
+}
+.widget_links a:hover, .widget_links a:focus, .widget_meta a:hover, .widget_meta a:focus, .widget_nav_menu a:hover, .widget_nav_menu a:focus, .widget_pages a:hover, .widget_pages a:focus, .widget_recent_comments a:hover, .widget_recent_comments a:focus, .widget_recent_entries a:hover, .widget_recent_entries a:focus, .widget_product_categories a:hover, .widget_product_categories a:focus {
+color: #f74a05;
+}
+
+.social-icons li a:hover {
+background: #f74a05;
+}
+
+.breadcrumb .breadcrumb-item > a:before, .breadcrumb .breadcrumb-item > a:hover, .breadcrumb .breadcrumb-item > a:focus {
+color: #f74a05;
+}
+
+.faq-item .faq-title > [class*="icon-"] {
+background-color: #f74a05;
+}
+.faq-item .faq-title:hover .title {
+color: #f74a05;
+}
+.faq-item .faq-title:hover .right-icon:after, .faq-item .faq-title:hover .right-icon:before {
+background-color: #f74a05;
+}
+
+.post .post-details .entry-header:before {
+background-color: #f74a05;
+}
+
+.post .entry-title a:hover, .post .entry-title a:focus {
+color: #f74a05;
+}
+.post .post-details .read-more:hover:before {
+border-color: transparent transparent transparent #f74a05;
+}
+
+.comment-respond .comment-reply-title:before {
+background-color: #f74a05;
+}
+
+.contact-info-item .shape-icon {
+color: #f74a05;
+}
+.contact-info-item:hover .shape-icon {
+background: #f74a05;
+}
+
+.form-controller:focus {
+border-color: #f74a05;
+}
+
+.user-register-area .form-header .form-subheading {
+color: #f74a05;
+}
+.user-register-area .remember-and-password .btn-password {
+color: #f74a05;
+}
+
+.btn-primary {
+background-color: #f74a05;
+border-color: #f74a05;
+}
+.btn-primary:focus, .btn-primary:active, .btn-primary:hover {
+color: #f74a05 !important;
+}
+
+.custom--table tbody tr {
+border-left: 1px solid #f74a054d;
+}
+.custom--table tr td, .custom--table tr th {
+border-bottom: 1px solid #f74a054d;
+border-right: 1px solid #f74a054d;
+}
+.custom--table .thead-dark th:not(:last-child) {
+border-right: 1px solid #f74a054d;
+}
+
+.custom--file-upload ~ label {
+background-color: #f74a05;
+}
+
+.auction-item-details .item-info-area .filter-info-tab .nav-tabs > li > a.active {
+background-color: #f74a05;
+}
+.auction-item-details .item-info-area .filter-info-tab .nav-tabs > li > a:hover {
+color: #f74a05;
+}
+.auction-item-details .item-bid-price-time .bid-timer {
+color: #f74a059e;
+}
+
+.section-heading:before {
+background-color: #f74a05;
+}
+
+.page-item.active .page-link {
+    background-color: #f74a05;
+    border-color: #f74a05;
+}
+.pagination .page-item a.active, .pagination .page-item a:hover, .pagination .page-item span.active, .pagination .page-item span:hover {
+    background: #f74a05;
+    border: 1px solid #f74a0533;
+}
+
+.carousel-nav-align-center .owl-nav > div {
+background-color: #f74a05;
+}
+
+.btn-links-area .btn-links:hover {
+background-color: #f74a05;
+}
+
+.about-info-list .single-info > .icon {
+background-color: #f74a05;
+}
+
+.profile-thumb .profilePicPreview {
+border: 3px solid #f74a05;
+}
+.profile-thumb .avatar-edit label {
+background-color: #f74a05;
+}
+
+.auction-item .heading a:hover {
+color: #f74a05;
+}
+
+</style>
